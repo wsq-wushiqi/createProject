2d1f07c8c36fccd5c9cfc1879d2184c1d32e1c27
吴诗琪 <1450573816@qq.com>
2021-05-10 16:27:07 +0800

    修改借口

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,4 +2,4 @@ import { fetch, post, put } from './http'
 
 export const login = (params) => post('/login', params)
 export const menu = () => fetch('/user')
-export const logout = (params) => post('/login/logout', params)
+export const logout = (params) => post('/logout', params)
